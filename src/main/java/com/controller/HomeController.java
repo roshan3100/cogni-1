f979bb70b79fa047745c539e82e5bef579c9ea70
Roshan Gulgulia <roshan.gulgulia.rg@gmail.com>
2019-03-27 15:50:03 +0530

    Add files via upload

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,26 @@
+
+package com.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		System.out.println("Home Page Requested, locale = " + locale);
+
+		return "roshan";
+	}
+}
